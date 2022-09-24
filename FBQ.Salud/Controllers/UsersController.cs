da89170db37f24831e502aa65c8a8bd21ac3ec53
claudiodamico <cdamico18@gmail.com>
2022-09-24 15:53:43 -0300

    Fixes

--- FBQ.Salud/Controllers/UsersController.cs
+++ FBQ.Salud/Controllers/UsersController.cs
@@ -5,125 +5,124 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FBQ.Salud_AccessData.Controllers
 {
-    public class UsersController
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
-        [Route("api/users")]
-        [ApiController]       
-        public class UserController : ControllerBase
+        private readonly IUserServices _service;
+        private readonly IMapper _mapper;
+
+        public UserController(IUserServices service, IMapper mapper)
         {
-            private readonly IUserServices _service;
-            private readonly IMapper _mapper;
+            _service = service;
+            _mapper = mapper;
+        }
 
-            public UserController(IUserServices service, IMapper mapper)
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
             {
-                _service = service;
-                _mapper = mapper;
+                var user = _service.GetAll();
+                var userMapped = _mapper.Map<List<UserDto>>(user);
+
+                return Ok(userMapped);
             }
-
-            [HttpGet]
-            public IActionResult GetAll()
+            catch (Exception e)
             {
-                try
-                {
-                    var user = _service.GetAll();
-                    var userMapped = _mapper.Map<List<UserDto>>(user);
-
-                    return Ok(userMapped);
-                }
-                catch (Exception e)
-                {
-                    return BadRequest(e.Message);
-                }
+                return BadRequest(e.Message);
             }
+        }
 
-            [HttpGet("{id}")]
-            public async Task<IActionResult> Get(int id)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
             {
-                try
+                var user = _service.GetUserById(id);
+                var userMapped = _mapper.Map<UserDto>(user);
+                if (user == null)
                 {
-                    var user = _service.GetUserById(id);
-                    var userMapped = _mapper.Map<UserDto>(user);
-                    if (user == null)
-                    {
-                        return NotFound("Usuario Inexistente");
-                    }
-                    return Ok(userMapped);
+                    return NotFound("Usuario Inexistente");
                 }
-                catch (Exception e)
-                {
-                    return BadRequest(e.Message);
-                }
+                return Ok(userMapped);
             }
-
-            [HttpPost]
-            public IActionResult CreateUser([FromForm] UserDto user)
+            catch (Exception e)
             {
-                try
-                {
-                    var userEntity = _service.CreateUser(user);
-
-                    if (userEntity != null)
-                    {
-                        var UserCreated = _mapper.Map<UserDto>(userEntity);
-                        return Ok("User Created");
-                    }
-
-                    return BadRequest();
-                }
-                catch (Exception e)
-                {
-                    return BadRequest(e.Message);
-                }
+                return BadRequest(e.Message);
             }
+        }
 
-            [HttpPut("{id}")]
-            public IActionResult UpdateUser(int id, UserDto user)
+        [HttpPost]
+        public IActionResult CreateUser([FromForm] UserDto user)
+        {
+            try
             {
-                try
+                var userEntity = _service.CreateUser(user);
+
+                if (userEntity != null)
                 {
-                    if (user == null)
-                    {
-                        return BadRequest("Completar todos los campos para realizar la actualizacion");
-                    }
-
-                    var userUpdate = _service.GetUserById(id);
-
-                    if (userUpdate == null)
-                    {
-                        return NotFound("Usuario Inexistente");
-                    }
-
-                    _mapper.Map(user, userUpdate);
-                    _service.Update(userUpdate);
-
-                    return Ok("Usuario actualizado");
+                    var UserCreated = _mapper.Map<UserDto>(userEntity);
+                    return Ok("User Created");
                 }
-                catch (Exception e)
-                {
-                    return BadRequest(e.Message);
-                }
+
+                return BadRequest();
             }
-
-            [HttpDelete("{id}")]
-            public IActionResult DeleteUser(int id)
+            catch (Exception e)
             {
-                try
-                {
-                    var user = _service.GetUserById(id);
+                return BadRequest(e.Message);
+            }
+        }
 
-                    if (user == null)
-                    {
-                        return NotFound("Usuario Inexistente");
-                    }
-
-                    _service.Delete(user);
-                    return Ok("Usuario eliminado");
-                }
-                catch (Exception e)
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, UserDto user)
+        {
+            try
+            {
+                if (user == null)
                 {
-                    return BadRequest(e.Message);
+                    return BadRequest("Completar todos los campos para realizar la actualizacion");
                 }
+
+                var userUpdate = _service.GetUserById(id);
+
+                if (userUpdate == null)
+                {
+                    return NotFound("Usuario Inexistente");
+                }
+
+                _mapper.Map(user, userUpdate);
+                _service.Update(userUpdate);
+
+                return Ok("Usuario actualizado");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            try
+            {
+                var user = _service.GetUserById(id);
+
+                if (user == null)
+                {
+                    return NotFound("Usuario Inexistente");
+                }
+
+                _service.Delete(user);
+                return Ok("Usuario eliminado");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
             }
         }
     }
-}
+}        
+        
+
