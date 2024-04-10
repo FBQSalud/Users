42abbcf74f286fa7f092c0dd2840123ba52d46d5
MarianoCarrizo <mariano.carrizo4280@gmail.com>
2024-04-10 18:45:48 -0300

    fixing bugs

--- FBQ.Salud/obj/Debug/net6.0/FBQ.Salud-Presentation.AssemblyInfo.cs
+++ FBQ.Salud/obj/Debug/net6.0/FBQ.Salud-Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FBQ.Salud-Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147d3ca0c1a1af5ea2cc0d2b8045ee7597bcf929")]
 [assembly: System.Reflection.AssemblyProductAttribute("FBQ.Salud-Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FBQ.Salud-Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
